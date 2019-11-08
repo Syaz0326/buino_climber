ddd35a4c9c13978327ea1cf736f12aafa8c5514e
Syaz0326 <tadakazu326@gmail.com>
2019-11-08 14:57:03 +0900

    feature: change window_img size

--- build/sketch/buino_climber.ino.cpp
+++ build/sketch/buino_climber.ino.cpp
@@ -17,16 +17,17 @@ void setup() {
 
 void loop() {
   if (gb.update()) {
-    int offset = 10;
+    int offset_left = 20;
+    int offset_top = 1;
     int margin = 3;
     for (size_t i = 0; i < 5; i++) {
-      int x = offset + (i * 10) + 3 * i;
-      int y = offset;
+      int x = offset_left + (i * 9) + 3 * i;
+      int y = offset_top;
       gb.display.drawBitmap(x, y, window_1);
-      gb.display.drawFastVLine(x + 12, 0, 84);
-      gb.display.drawFastVLine(x + 10, 0, 84);
+      gb.display.drawFastVLine(x - 1, 0, 84);
+      gb.display.drawFastVLine(x + 9, 0, 84);
     }
-    gb.display.drawBitmap(30, 30, buino_up_0);
+    gb.display.drawBitmap(44, 30, buino_up_0);
   }
 }
 
