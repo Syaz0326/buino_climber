c99833875bdb64b8a7fe1495bff1c3cda3d1cf26
Syaz0326 <tadakazu326@gmail.com>
2019-11-08 13:23:17 +0900

    chore: add Bitmaps.h

--- buino_climber.ino
+++ buino_climber.ino
@@ -1,5 +1,6 @@
 #include <SPI.h>
 #include <Gamebuino.h>
+#include "Bitmaps.h"
 Gamebuino gb;
 
 void setup() {
@@ -9,6 +10,15 @@ void setup() {
 
 void loop() {
   if (gb.update()) {
-    gb.display.println(F("Buino Climber"));
+    int offset = 10;
+    int margin = 3;
+    for (size_t i = 0; i < 5; i++) {
+      int x = offset + (i * 10) + 3 * i;
+      int y = offset;
+      gb.display.drawBitmap(x, y, window_1);
+      gb.display.drawFastVLine(x + 12, 0, 84);
+      gb.display.drawFastVLine(x + 10, 0, 84);
+    }
+    gb.display.drawBitmap(30, 30, buino_up_0);
   }
 }
